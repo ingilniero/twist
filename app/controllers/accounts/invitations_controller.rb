4c0ffbb519c3db37cf678d01840a657afd64a15b
Gilberto Villa <ingilniero@gmail.com>
2020-02-22 15:32:09 -0600

    Allow account owners to remove users from account

--- app/controllers/accounts/invitations_controller.rb
+++ app/controllers/accounts/invitations_controller.rb
@@ -48,11 +48,5 @@ module Accounts
       params.require(:invitation).permit(:email)
     end
 
-    def authorize_owner!
-      unless owner?
-        flash[:alert] = "Only an owner of an account can do that."
-        redirect_to root_url(subdomain: current_account.subdomain)
-      end
-    end
   end
 end
