28c51d3140da118555152384af6eb400a2b9c3b6
Gilberto Villa <ingilniero@gmail.com>
2020-02-22 07:10:21 -0600

    Users can now accept invitations to an account

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,5 +4,7 @@ class Account < ApplicationRecord
 
   validates :subdomain, presence: true, uniqueness: true
 
+  has_many :memberships
+  has_many :users, through: :memberships
   has_many :invitations
 end
