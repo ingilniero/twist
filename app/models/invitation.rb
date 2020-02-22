453065a28ca1ac95ab1487d4529cdc165249439b
Gilberto Villa <ingilniero@gmail.com>
2020-02-22 07:18:02 -0600

    Use random tokens for invitation identifiers

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,4 +2,16 @@ class Invitation < ApplicationRecord
   belongs_to :account
 
   validates :email, presence: true
+
+  before_create :generate_token
+
+  def to_param
+    token
+  end
+
+  private
+
+  def generate_token
+    self.token = SecureRandom.hex(16)
+  end
 end
